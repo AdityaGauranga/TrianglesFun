a726066e40e014a431634a720d0120de97d64577
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-09 21:30:33 +0530

    document queries added

---
+++ quiz.js
@@ -0,0 +1,4 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelectorAll("#submit-answer-btn");
+const outputEl = document.querySelector("#output");
+
